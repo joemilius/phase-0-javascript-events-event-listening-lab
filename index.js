e211de36e22f63a2a384f0fdf49b079805456f2f
JoeMilius <joemilius@gmail.com>
2021-05-20 06:25:12 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function thereIsNoTry(){
+        alert("May the force be with you.");
+    };
+    input.addEventListener('click', thereIsNoTry);
 }
+addingEventListener();
