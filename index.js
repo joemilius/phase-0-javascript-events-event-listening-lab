98d3b24597c7f01e43310bb08c46686051629c1e
Joe Milius <joemilius@gmail.com>
2024-02-29 14:55:58 -0700

    added submit button to form and reverted solution to original code

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 
 
 function addingEventListener() {
-    const input = document.getElementById('input');
-    function thereIsNoTry(){
-        alert("May the force be with you.");
-    };
-    input.addEventListener('click', thereIsNoTry);
+    
 }
 addingEventListener();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.getElementById('input');
+//     function thereIsNoTry(){
+//         alert("May the force be with you.");
+//     };
+//     input.addEventListener('click', thereIsNoTry);
