4dcec0c3893837a658ae80988b1a6f082fbda558
Joe Milius <joemilius@gmail.com>
2024-02-29 17:47:58 -0700

    solution

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 
 
 function addingEventListener() {
+    const input = document.getElementById('button');
     
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
 addingEventListener();
 
+let inputValue = ''
+const formInput = document.getElementById('form-input')
 
+formInput.addEventListener('change', function(event){
+  inputValue = event.target.value
+})
 
+const form = document.getElementById('form')
+form.addEventListener('submit', function(event){
+   event.preventDefault()
+   const li = document.createElement('li')
+   li.textContent = inputValue 
+   const ul = document.getElementById("text-container")
+   ul.append(li)
+})
 
 
 
